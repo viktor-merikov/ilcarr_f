a39492488fdae865739dafec16130744122bd7d8
Viktor <nk070987mvv@gmail.com>
2019-10-11 08:59:52 +0300

    changes in search component

--- src/app/cars/search/search.component.ts
+++ src/app/cars/search/search.component.ts
@@ -1,18 +1,23 @@
 import {Component, OnInit} from '@angular/core';
 
+export interface CarCoord {
+  latitude: number;
+  longitude: number;
+}
+
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
 
+export class SearchComponent implements OnInit {
   priceRange = 50;
   mapActive = false;
   searchActive = true;
   filtersActive = false;
-  latitude = 32.2970637;
-  longitude = 34.85437006;
+  carPoint: CarCoord = {latitude: 32.2970637, longitude: 34.85437006};
 
   constructor() {
   }
@@ -20,22 +25,21 @@ export class SearchComponent implements OnInit {
   ngOnInit() {
   }
 
-
-  mapIsActive() {
-    this.searchActive = false;
-    this.filtersActive = false;
-    this.mapActive = true;
-  }
-
-  searchIsActive() {
-    this.searchActive = true;
-    this.filtersActive = false;
-    this.mapActive = false;
-  }
-
-  filtersIsActive() {
-    this.searchActive = false;
-    this.filtersActive = true;
-    this.mapActive = false;
+  // What window is active
+  private wwIsActive(window: string) {
+    if (window === 'filters') {
+      this.mapActive = false;
+      this.searchActive = false;
+      this.filtersActive = true;
+    } else if (window === 'map') {
+      this.mapActive = true;
+      this.searchActive = false;
+      this.filtersActive = false;
+    } else {
+      this.mapActive = false;
+      this.searchActive = true;
+      this.filtersActive = false;
+    }
   }
 }
+
